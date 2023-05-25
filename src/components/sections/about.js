664ab09afa1aa3895fed36c6a9b1522037008bea
Diego Barreiro <diego.barreiro.perez@gmail.com>
2023-05-25 20:29:11 +0200

    Update about

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['Python', 'Java', 'Typescript (ES6+)', 'PHP', 'C & Lua', 'AWS & GCP'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,33 +135,31 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Diego Barreiro Pérez and I love creating things that live on the
+              internet. With several years of programming under my belt, I've built a strong
+              foundation in both small and large-scale projects.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              I've always had a relentless drive to push my skills to the next level, aiming to
+              explore new opportunities, such as becoming{' '}
+              <a href="https://www.kodular.io">a company founder</a>, taking an internship at a{' '}
+              <a href="https://amazon.jobs/en/teams/time-and-attendance">big-tech company</a> and
+              work on some <a href="/archive">other personal projects</a>.
             </p>
 
             <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              Currently pursuing a Bachelor's degree in Computer Engineering in my home city, I've
+              also had the amazing opportunity to go on an exchange program and gain practical
+              experience through a summer project at{' '}
+              <a href="https://summerofcode.withgoogle.com/archive/2020/projects/5789768257896448">
+                MIT App Inventor
+              </a>
+              , and contribute to the development of my home town by{' '}
+              <a href="https://proteccioncivil.sdc.gal">volunteering</a>.
             </p>
 
-            <p>Here are a few technologies I’ve been working with recently:</p>
+            <p>Here are a few technologies I've been working with recently:</p>
           </div>
 
           <ul className="skills-list">
