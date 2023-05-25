dcf85a5e6039a33c7968d49ec922d41d31e5de19
Diego Barreiro <diego.barreiro.perez@gmail.com>
2023-05-25 18:33:54 +0200

    Update hero

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,32 +60,26 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Diego Barreiro.</h2>;
+  const three = <h3 className="big-heading">I code for the digital realm.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I'm a software engineer focused on distributed systems and reliable web development,
+        designing scalable architectures aiming for the best digital fault-tolerance experience.
+        Currently, I'm finishing my Computer Engineering degree working on{' '}
+        <a
+          href="https://github.com/barreeeiroo/TFG-Timetable-Solvers-ASP"
+          target="_blank"
+          rel="noreferrer">
+          my final thesis
         </a>
         .
       </p>
     </>
   );
-  const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
-    </a>
-  );
 
-  const items = [one, two, three, four, five];
+  const items = [one, two, three, four];
 
   return (
     <StyledHeroSection>
