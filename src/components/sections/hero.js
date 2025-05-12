a9ccf5d11b370663148cfc46b018851ae3286eb3
Diego Barreiro Perez <diego@barreiro.xyz>
2025-05-12 02:17:09 +0200

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,7 +60,7 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Diego Barreiro.</h2>;
+  const two = <h2 className="big-heading">Diego Barreiro Perez.</h2>;
   const three = <h3 className="big-heading">I code for the digital realm.</h3>;
   const four = (
     <>
