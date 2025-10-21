314fe2f1d8aed19818acfb55b8f77752cfc07ac6
Diego Barreiro Perez <diegobp@amazon.com>
2025-10-21 11:01:24 +0100

    Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,21 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  site: 'https://diego-dev.barreiro.dev',
+  integrations: [
+    icon(),
+    sitemap({
+      lastmod: new Date()
+    })
+  ],
+  build: {
+    inlineStylesheets: 'never'
+  },
+  compressHTML: true,
   vite: {
     plugins: [tailwindcss()]
   }
